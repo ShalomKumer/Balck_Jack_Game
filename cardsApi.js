c1507a707fef0e1e76447a1edda1304e1b9958fc
isravra <imcosten26@gmail.com>
2025-05-13 14:28:21 +0300

    api

---
+++ cardsApi.js
@@ -0,0 +1,8 @@
+const dealerCardsDiv = document.getElementById("dealerCards");
+const playerCardsDiv = document.getElementById("playerCards");
+const addCardsBtn = document.getElementById("addCards");
+const stopAddBtn = document.getElementById("stopAdd");
+
+addCardsBtn.addEventListener('click', () => {
+    
+})
