7582ef97ccfeb8c21adac0449b76dd6a22eba783
ShalomKumer <kumersb@gmail.com>
2025-05-13 14:30:39 +0300

    יצרתי את המחלקה

---
+++ Game.js
@@ -0,0 +1,27 @@
+class Game{
+    constructor(){
+        const playerCards = [];
+        const dilerCards = [];
+        const gameStatus = 0;
+    }
+    //  יוצר את המשחק  - 2 קלפים ללקוח  ו קלף אחד לדילר
+    prepareGame(){
+
+    }
+    // הגדרה של תור של הלקוח 
+    playerTurn(){
+
+    }
+    // ניצחון של הלקוח 
+    playerWon(){
+
+    }
+    dilerTurn(){
+
+    }
+    calculateWin(){
+
+    }
+
+
+}
